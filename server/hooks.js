6d707d940aa37241457f744535521f08ce986c82
obonyojimmy <cliffjimmy27@gmail.com>
2020-04-25 03:45:48 +0300

    Connect Parent -> Child spans.

--- server/hooks.js
+++ server/hooks.js
@@ -1,18 +1,21 @@
 import { Meteor } from 'meteor/meteor';
 import { tracer, exporter } from './tracing';
 
+let connectedSpans = [];
+
 Meteor.onConnection(function (connection) {
-  const { id, onClose, clientAddress, httpHeaders } = connection;
-  // console.log({ httpHeaders });
+  const { id, onClose, clientAddress } = connection;
 
   // start trace-spans
   const mainSpan = tracer.startSpan('connected');
+  connectedSpans.push({ id, span: mainSpan });
 
   mainSpan.setAttribute('clientAddress', clientAddress);
   mainSpan.setAttribute('connectionId', id);
 
   onClose(function () {
     mainSpan.end();
+    connectedSpans = connectedSpans.filter(s => s.id !== id);
     // exporter.shutdown();
   });
 });
@@ -24,22 +27,35 @@ Meteor.server.method_handlers = Object.entries(originalMethods)
     return {
       ...prev,
       [name]: function () {
-        const parentSpan = tracer.getCurrentSpan();
-        console.log(parentSpan);
-        const span = tracer.startSpan(`Method::${name}`);
-        const { userId } = this;
+        const { connection: { id } } = this;
 
-        if (userId) {
-          span.setAttribute('userId', userId);
-        }
+        const parentSpan = connectedSpans.find(s => s.id === id);
+        const span = tracer.startSpan(`Method::${name}`, { parent: parentSpan.span, kind: 1 });
 
+        span.setAttribute('connectionId', id);
 
         try {
           span.addEvent(`invoking ${name}`);
           const res = handler.apply(this, arguments);
+
+          const userId = Meteor.userId();
+          if (userId) {
+            const { profile, emails } = Meteor.user();
+
+            span.setAttribute('userId', userId);
+            if (profile) {
+              span.setAttribute('userProfile', profile);
+            }
+            if (emails) {
+              span.setAttribute('userEmails', emails);
+            }
+
+          }
+
           span.end();
           return res;
         } catch (error) {
+          const userId = Meteor.userId();
           const { message /* stack */ } = error;
           console.log({ userId, name, message });
           span.end();
